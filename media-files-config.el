174569a9f9f4a844fcb9baa7030b1bf5373a70a9
Philip Weaver <philip.weaver@gmail.com>
2010-11-18 22:13:07 -0800

    update config
    Conflicts:
    
    	media-files-config.el

--- media-files-config.el
+++ media-files-config.el
@@ -6,7 +6,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar phil/series-names
-  '("Big Love" "Cleveland Show" "Bored to Death" "Star Wars Clone Wars" "Family Guy" "Colbert Report" "Daily Show" "Being Erica" "Breaking Bad" "Big Love" "Caprica" "Dexter" "Futurama" "Mad Men" "South Park" "Weeds" "Dollhouse" "Carnivale" "Curb Your Enthusiasm" "Deadwood" "John Adams" "Malcolm in the Middle" "Star Trek Voyager" "Star Trek DS9" "Star Trek The Next Generation" "The 4400" "The Shield" "The Wire" "Sister Wives" "The Walking Dead" "The Wonder Years" "Veronica Mars" "Sex and the City" "X-Files" "Simpsons" "Bones" "Extras" "Firefly" "Scrubs" "Rome" "Battlestar Galactica" "Californication" "Angel"))
+  '("Big Love" "Cleveland Show" "Bored to Death" "Star Wars Clone Wars" "Family Guy" "Colbert Report" "Daily Show" "Being Erica" "Breaking Bad" "Big Love" "Caprica" "Dexter" "Futurama" "Mad Men" "South Park" "Weeds" "Dollhouse" "Carnivale" "Curb Your Enthusiasm" "Deadwood" "John Adams" "Malcolm in the Middle" "Star Trek Voyager" "Star Trek DS9" "Star Trek The Next Generation" "The 4400" "The Shield" "Rubicon" "The Wire" "Sister Wives" "The Walking Dead" "The Wonder Years" "Veronica Mars" "Sex and the City" "X-Files" "Simpsons" "Bones" "Extras" "Firefly" "Stargate Universe" "Scrubs" "Rome" "Battlestar Galactica" "Californication" "Angel" "Conan"))
 
 ;; TODO The 4400 needs a special regexp. "The 4400.408" is identified as season
 ;; 44, episode 0
@@ -39,7 +39,7 @@
 
   (setq media-files-command-path
         (cond ((memq system-type '(windows-nt cygwin))
-               "c:/Program Files/VideoLAN/VLC/vlc")
+               "c:/Program Files/VideoLAN/VLC/vlc.exe")
               ((eq system-type 'darwin)
                "open")
               (t '("vlc" "mplayer" "totem"))))
